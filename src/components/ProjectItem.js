88c16f2d134a2ab0ed3589c28781951b7a0ecdc5
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-20 13:10:35 +0300

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const spans = technologies.map((tech, index) => (
+    <span key={index}>{tech}</span>
+  ));
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{spans}</div>
     </div>
   );
 }
